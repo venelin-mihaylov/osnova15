07fa1a1718c381f8da4d4c836fa3b345e736870f
venelin <venelin.mihaylov@gmail.com>
2016-07-15 20:26:19 +0300

    * work on migrations & seed/fixture

--- migrations/20160715144001_migration01.js
+++ migrations/20160715144001_migration01.js
@@ -11,10 +11,66 @@ exports.up = function(knex, Promise) {
       t.string('firstName').notNullable()
       t.string('lastName').notNullable()
       t.string('email').notNullable()
-    })
+    }),
+    knex.schema.createTable('tournament', function(t) {
+      t.increments('id').primary()
+      t.string('name').notNullable()
+      t.string('description')
+      t.jsonb('notes')
+    }),
+    knex.schema.createTable('matches', function(t) {
+      t.increments('id').primary()
+      t.string('name').notNullable()
+      t.string('description')
+      t.integer('tournamentId').notNullable().references('id').inTable('tournament')
+    }),
+    knex.schema.createTable('match_competitor', function(t) {
+      t.increments('id').primary()
+      t.boolean('disqualified').notNullable().default(false)
+      t.string('notes')
+      t.integer('matchId').notNullable().references('id').inTable('matches')
+      t.integer('competitorId').notNullable().references('id').inTable('competitor')
+    }),
+    knex.schema.createTable('exercise', function(t) {
+      t.increments('id').primary()
+      t.boolean('favourite')
+      t.string('name')
+    }),
+    knex.schema.createTable('match_exercise', function(t) {
+      t.increments('id').primary()
+      t.integer('matchId').notNullable().references('id').inTable('matches')
+      t.integer('exerciseId').notNullable().references('id').inTable('exercise')
+    }),
+    knex.schema.createTable('target', function(t) {
+      t.increments('id').primary()
+      t.boolean('favourite').notNullable().default(false)
+      t.string('name').notNullable()
+    }),
+    knex.schema.createTable('target_zone', function(t) {
+      t.increments('id').primary()
+      t.string('name').notNullable()
+      t.integer('score').notNullable()
+      t.integer('targetId').notNullable().references('id').inTable('target')
+    }),
+    knex.schema.createTable('exercise_target', function(t) {
+      t.increments('id').primary()
+      t.integer('exerciseId').notNullable().references('id').inTable('exercise')
+      t.integer('targetId').notNullable().references('id').inTable('target')
+    }),
   ])
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.raw('DROP TABLE users CASCADE'),
+    knex.raw('DROP TABLE competitor CASCADE'),
+    knex.raw('DROP TABLE tournament CASCADE'),
+    knex.raw('DROP TABLE matches CASCADE'),
+    knex.raw('DROP TABLE match_competitor CASCADE'),
+    knex.raw('DROP TABLE exercise CASCADE'),
+    knex.raw('DROP TABLE match_exercise CASCADE'),
+    knex.raw('DROP TABLE target CASCADE'),
+    knex.raw('DROP TABLE target_zone CASCADE'),
+    knex.raw('DROP TABLE exercise_target CASCADE')
+  ])
 };
--- seeds/seed1.js
+++ seeds/seed1.js
@@ -1,12 +1,87 @@
+exports.seed = function (knex, Promise) {
 
-exports.seed = function(knex, Promise) {
+  function randInt(low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
+  }
+
+  let arr = []
+
+
+  arr.push(knex('match_exercise').del())
+  arr.push(knex.raw('ALTER SEQUENCE tournament_id_seq RESTART WITH 1'))
+  arr.push(knex('target').del())
+  arr.push(knex.raw('ALTER SEQUENCE target_id_seq RESTART WITH 1'))
+  arr.push(knex('exercise').del())
+  arr.push(knex.raw('ALTER SEQUENCE exercise_id_seq RESTART WITH 1'))
+  arr.push(knex('match_competitor').del())
+  arr.push(knex.raw('ALTER SEQUENCE match_competitor_id_seq RESTART WITH 1'))
+  arr.push(knex('matches').del())
+  arr.push(knex.raw('ALTER SEQUENCE matches_id_seq RESTART WITH 1'))
+  arr.push(knex('competitor').del())
+  arr.push(knex.raw('ALTER SEQUENCE competitor_id_seq RESTART WITH 1'))
+  arr.push(knex('tournament').del())
+  arr.push(knex.raw('ALTER SEQUENCE tournament_id_seq RESTART WITH 1'))
+
+
+  for (let i = 0; i < 100; i++) {
+    arr.push(knex('tournament').insert({name: 'tournament ' + i}))
+  }
+
+  const firstNames = ['Yavor', 'Georgi', 'Ivelin', 'Venelin', 'Petar']
+  const lastNames = ['Mihaylov', 'Dobrev', 'Ivanov', 'Yasenov']
+  let firstName, lastName, email
+  for (let i = 0; i < 100; i++) {
+    firstName = firstNames[randInt(0, firstNames.length - 1)]
+    lastName = lastNames[randInt(0, lastNames.length - 1)]
+    email = firstName + '@' + lastName + '.com'
+
+    arr.push(knex('competitor').insert({firstName, lastName, email}))
+  }
+
+
+  for (let i = 0; i < 1000; i++) {
+    arr.push(knex('matches').insert({
+      name: 'match ' + i,
+      tournamentId: randInt(1, 99)
+    }))
+  }
+
+  arr.push(knex('match_competitor').del())
+  for (let i = 0; i < 10000; i++) {
+    arr.push(knex('match_competitor').insert({
+      competitorId: randInt(1, 99),
+      matchId: randInt(1, 999)
+    }))
+  }
+
+  for (let i = 0; i < 10; i++) {
+    arr.push(knex('exercise').insert({name: 'exercise ' + i}))
+  }
+
+  for (let i = 0; i < 100; i++) {
+    arr.push(knex('match_exercise').insert({
+      matchId: randInt(1, 10),
+      exerciseId: randInt(1, 10)
+    }))
+  }
+
+  for (let i = 0; i < 100; i++) {
+    arr.push(knex('target').returning('id').insert({
+      name: 'target ' + i
+    }).then(function(arrTargetId) {
+      return knex('target_zone').insert({
+        targetId: arrTargetId[randInt(0, arrTargetId.length - 1)],
+        name: 'target zone ' + randInt(0, 999999),
+        score: randInt(0, 10)
+      })
+    }))
+  }
+
+
+
   return Promise.join(
-    // Deletes ALL existing entries
-    knex('table_name').del(),
-
-    // Inserts seed entries
-    knex('table_name').insert({id: 1, colName: 'rowValue'}),
-    knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-    knex('table_name').insert({id: 3, colName: 'rowValue3'})
-  );
-};
+    knex('users').del(),
+    knex('users').insert({email: 'v@v.com', password: '123asd'}),
+    Promise.all(arr)
+  )
+}
