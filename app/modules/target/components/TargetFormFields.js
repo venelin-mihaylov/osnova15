72c7df4411769e7143cc68d54d01edcbf9a08259
Venelin Mihaylov <venelin@enforcer.bg>
2016-07-18 15:55:12 +0300

    add FieldSet for 1:N, change +/- for 1:N to be plus/minus icon buttons

--- app/modules/target/components/TargetFormFields.js
+++ app/modules/target/components/TargetFormFields.js
@@ -1,4 +1,4 @@
-"use strict"
+'use strict'
 import React from 'react'
 import OsnovaTextField from 'components/OsnovaTextField'
 import {actions} from 'react-redux-form'
@@ -9,6 +9,7 @@ import TextField from 'material-ui/TextField'
 import MaterialCheckbox from 'components/MaterialCheckbox'
 import ActionFavorite from 'material-ui/svg-icons/action/favorite';
 import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
+import IconButton from 'material-ui/IconButton'
 
 export const TargetFormFields = ({
   dispatch,
@@ -22,50 +23,55 @@ export const TargetFormFields = ({
   <div>
     <OsnovaTextField
       {...{form, entity}}
-      field="name"
+      field='name'
       required
-      hintText="name"
-      floatingLabelText="name"
+      hintText='name'
+      floatingLabelText='name'
     />
-
     <MaterialField model={rrfField(entity, 'favourite')}>
       <MaterialCheckbox
+        labelPosition='right'
         checkedIcon={<ActionFavorite />}
         uncheckedIcon={<ActionFavoriteBorder />}
-        label="Favourite"
+        label='Favourite'
       />
     </MaterialField>
 
     <br/>
 
-    <RaisedButton label="Add zone" style={{margin: 5}} onClick={() => {
-      dispatch(actions.push(rrfField(entity, 'target_zone[]'), {
-        text: ''
-      }))
+
+    <RaisedButton label='Remove zone' style={{margin: 5}} onClick={() => {
+
     }}/>
-    <RaisedButton label="Remove zone" style={{margin: 5}} onClick={() => {
-      dispatch(actions.remove(rrfField(entity, 'target_zone[]'), target_zone ? target_zone.length - 1 : 0))
-    }}/>
+    <If condition={target_zone}>
+      <fieldset style={{borderTop: '1px solid green', padding: '10px'}}>
+        <legend>
+          <IconButton iconClassName="fa fa-plus" onClick={() => dispatch(actions.push(rrfField(entity, 'target_zone[]'), {}))}/>
+          <IconButton iconClassName="fa fa-minus" onClick={() => dispatch(actions.remove(rrfField(entity, 'target_zone[]'), target_zone ? target_zone.length - 1 : 0))}/>
+          <h2 style={{marginRight: '10px', display: 'inline'}}>Target zones</h2>
+        </legend>
     {target_zone && target_zone.map((n, i) => (
       <div>
         <MaterialField key={`${i}.name`} model={rrfField(entity, `target_zone[${i}].name`)}>
           <TextField
             required
-            hintText="Name"
-            floatingLabelText="Name"
+            hintText='Name'
+            floatingLabelText='Name'
             errorText={MUIErrorText(form, entity, `target_zone[${i}].name`)}
           />
         </MaterialField>
         <MaterialField key={`${i}.score`} model={rrfField(entity, `target_zone[${i}].score`)}>
           <TextField
             required
-            hintText="Score"
-            floatingLabelText="Score"
+            hintText='Score'
+            floatingLabelText='Score'
             errorText={MUIErrorText(form, entity, `target_zone[${i}].score`)}
           />
         </MaterialField>
       </div>
     ))}
+      </fieldset>
+    </If>
   </div>
 )
 export default TargetFormFields
