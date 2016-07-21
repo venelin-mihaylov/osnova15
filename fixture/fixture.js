a862f12fc315e1b714440dd574d8c42022f22c46
Venelin Mihaylov <venelin@enforcer.bg>
2016-07-21 09:53:13 +0300

    work on fixtures

--- fixture/fixture.js
+++ fixture/fixture.js
@@ -1,46 +1,65 @@
 import sqlFixtures from 'sql-fixtures'
 import dbConfig from '../api/config/db'
+import _range from 'lodash.range'
 
 function generate(cnt, data) {
-  let ret = []
-  let r = {}
-
-  const fields = Object.keys(data)
-  for (let i = 0; i < cnt; i++) {
-    r = {}
-    fields.forEach(f => {
+  return _range(cnt).map((i) => {
+    let r = {}
+    Object.keys(data).forEach(f => {
       if (typeof data[f] == 'function') {
         r[f] = data[f](i, r)
       } else {
         r[f] = data[f]
       }
     })
-    ret.push(r)
-  }
-  return ret
+    return r
+  })
 }
 
 function randInt(low, high) {
   return Math.floor(Math.random() * (high - low + 1) + low);
 }
 
+function arrUniqRandInt(low, high, cnt) {
+  let ret = {}
+  _range(cnt).forEach(() => ret[randInt(low, high)] = true)
+  return Object.keys(ret)
+}
+
 const firstNames = ['Yavor', 'Georgi', 'Ivelin', 'Venelin', 'Petar']
 const lastNames = ['Mihaylov', 'Dobrev', 'Ivanov', 'Yasenov']
 
+const count = {
+  tournament: 10,
+  competitor: 10,
+  target: 10,
+  matches: 10,
+  match_competitor_max: 5, // per match
+}
+
 const dataSpec = {
 
-  tournament: generate(10, {name: (i) => `Tournament ${i}`}),
-  competitor: generate(10, {
+  tournament: generate(count.tournament, {name: (i) => `Tournament ${i}`}),
+  competitor: generate(count.competitor, {
     firstName: () => firstNames[randInt(0, firstNames.length - 1)],
     lastName: () => lastNames[randInt(0, lastNames.length - 1)],
     email: (i, r) => r.firstName + '@' + r.lastName + '.com'
   }),
-  target: generate(10, {name: (i) => `Target ${i}`}),
-  matches: generate(10, {
+  target: generate(count.target, {name: (i) => `Target ${i}`}),
+  matches: generate(count.matches, {
     name: (i) => `Match ${i}`,
-    tournamentId:  (i) => `tournament:${i}`
+    tournamentId:  () => `tournament:${randInt(1, count.tournament)}`
+  }),
+  match_competitor: _range(0, count.matches).map((matchIdx) => {
+    const arr = arrUniqRandInt(0, count.competitor-1, count.match_competitor_max)
+    return generate(arr.length, {
+      matchId: `matches:${matchIdx}`,
+      competitorId: (i) => `competitor:${arr[i-1]}`
+    })
   })
 }
+
+console.log(JSON.stringify(dataSpec.match_competitor, null, 2))
 
 const options = {
   showWarning: true
