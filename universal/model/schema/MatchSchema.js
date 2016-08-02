a3650e9140d524188ee651b6d3ff9021a9dec710
Venelin Mihaylov <venelin@enforcer.bg>
2016-08-02 14:31:15 +0300

    * match fields

--- universal/model/schema/MatchSchema.js
+++ universal/model/schema/MatchSchema.js
@@ -4,14 +4,33 @@ const MatchSchema = {
   properties: {
     id: {type: 'integer'},
     name: {type: 'string', minLength: 10, maxLength: 255},
+    description: {type: 'string'},
     tournamentId: {type: 'integer', labelField: "name"},
-    notes: {
-      type: ['array', 'object', 'null'],
-      required: ['text'],
-      properties: {
-        text: {type: 'string'}
+    discipline: {
+      type: 'integer',
+      enum: [1, 2, 3],
+      enumProps: {
+        1: 'T-class tactical sniper',
+        2: 'T-class ultra long range',
+        3: 'T-class rimfire',
+        4: 'T-class multigun'
       }
-    }
+    },
+    type: {
+      type: 'integer',
+      enum: [1,2],
+      enumProps: {
+        1: 'Individual',
+        2: 'Team/Individual'
+      }
+    },
+    startDate: {type: 'string', format: 'date-time', subtype: 'date'},
+    endDate: {type: 'string', format: 'date-time', subtype: 'date'},
+    organizer: {type: 'string'},
+    country: {type: 'string'},
+    rangeMaster: {type: 'string'},
+    statMaster: {type: 'string'},
+    matchDirector: {type: 'string'},
   }
 }
 export default MatchSchema
