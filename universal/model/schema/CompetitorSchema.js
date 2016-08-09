d710fc679309f049ef0d227966cfc84e5a31b668
Venelin Mihaylov <venelin@enforcer.bg>
2016-08-09 10:39:26 +0300

    * fix birthDate for competitor

--- universal/model/schema/CompetitorSchema.js
+++ universal/model/schema/CompetitorSchema.js
@@ -7,7 +7,7 @@ const CompetitorSchema = {
     firstName: {type: 'string', maxLength: 255},
     lastName: {type: 'string', maxLength: 255},
     nickName: {type: 'string', maxLength: 255},
-    birthDate: {type: 'string', maxLength: 255, format: 'date-time', subtype: 'date'},
+    birthDate: {type: 'string', maxLength: 255, format: 'date', subtype: 'date'},
     email: {type: 'string', maxLength: 255, format: 'email'},
     phone: {type: 'string', maxLength: 255},
     country: {type: 'string', maxLength: 255},
