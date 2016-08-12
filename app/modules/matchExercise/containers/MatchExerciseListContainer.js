4b2a89bb2338d65ba5da85723c52312194228e4e
Venelin Mihaylov <venelin@enforcer.bg>
2016-08-12 13:29:06 +0300

    * do not use state to add a new exercise to a match

--- app/modules/matchExercise/containers/MatchExerciseListContainer.js
+++ app/modules/matchExercise/containers/MatchExerciseListContainer.js
@@ -6,7 +6,6 @@ import EntityList from "components/EntityList"
 import HasSelectionHOC from 'hoc/HasSelectionHOC'
 import OsnovaListContainer from 'components/OsnovaListContainer'
 import FKSelect from 'components/FKSelect'
-import FKAct from 'constants/FKAct'
 import CRUDAct from 'constants/CRUDAct'
 import axios from 'axios'
 
