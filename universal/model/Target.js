150456f00d3f8985a0a7bcfd04134a54e697f608
venelin <venelin.mihaylov@gmail.com>
2016-09-13 18:58:12 +0300

    * work on target image thumbnail

--- universal/model/Target.js
+++ universal/model/Target.js
@@ -1,5 +1,5 @@
 import OsnovaModel from './OsnovaModel'
-import TargetSchema from "../model/schema/TargetSchema"
+import TargetSchema from '../model/schema/TargetSchema'
 import TargetRelations from './relations/TargetRelations'
 import {toObjectionRelations} from './util/util'
 
@@ -7,4 +7,15 @@ export default class Target extends OsnovaModel {
   static tableName = 'target'
   static jsonSchema = TargetSchema
   static relationMappings = toObjectionRelations(TargetRelations)
+}
+
+function stripHtmlImgPrefix(data) {
+  return data.replace('data:image/jpeg;base64,', '')
+}
+
+Target.prototype.$beforeUpdate = function (opt, context) {
+  console.log(this)
+  console.log('beforeUpdate')
+  console.log(opt)
+  console.log(context)
 }
