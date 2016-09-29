3bac78b73b73d57a357519bf963583603e0b83ae
Venelin Mihaylov <venelin@enforcer.bg>
2016-09-29 14:31:02 +0300

    * work on scoring

--- app/reducers/matchExerciseTargetZone.js
+++ app/reducers/matchExerciseTargetZone.js
@@ -1,9 +1,22 @@
-import {rrfModel, rrfField} from 'utils/Util'
+import {rrfModel} from 'utils/Util'
 import {targetZoneScore} from '../../universal/scoring/scoring'
 import find from 'lodash/find'
 import get from 'lodash/get'
-import clone from 'lodash/clone'
 import i from 'icepick'
+
+const setScoreOnWeightChange = function (entity, model, value, state) {
+  const regex = `${rrfModel(entity)}\\[(\\d+)\\].*weight`
+  const match = model.match(new RegExp(regex))
+  if (match) {
+    const idx = match[1]
+    const cur = get(state, model)
+    if (cur !== value) {
+      const score = targetZoneScore(100, 100, value)
+      return i.assocIn(state, [idx, 'score'], score)
+    }
+  }
+  return state
+}
 
 export default function matchExerciseTargetZone(state = [], action = {}) { // eslint-disable-line
   const {
@@ -13,22 +26,14 @@ export default function matchExerciseTargetZone(state = [], action = {}) { // es
     ...act
   } = action
 
+  let change = null
+
   const entity = 'matchExerciseTargetZone'
   switch (type) {
     case 'rrf/change':
-      const regex = `${rrfModel(entity)}\\[(\\d+)\\].*weight`
-      const match = model.match(new RegExp(regex))
-      if (match) {
-        const idx = match[1]
-        const cur = get(state, model)
-        if (cur !== value) {
-          const score = targetZoneScore(100, 100, value)
-          return i.assocIn(state, [idx, 'score'], score)
-        }
-      }
-      return state
+      return setScoreOnWeightChange(entity, model, value, state)
     case 'rrf/batch':
-      const change = find(act.actions, {type: 'rrf/change'})
+      change = find(act.actions, {type: 'rrf/change'})
       if (change) {
         return matchExerciseTargetZone(state, change)
       }
