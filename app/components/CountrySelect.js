d8877a4cace26b7b3b9aaf04e80a404e587cf724
Venelin Mihaylov <venelin@enforcer.bg>
2016-10-18 13:50:53 +0300

    * new CountrySelect component based on Search

--- app/components/CountrySelect.js
+++ app/components/CountrySelect.js
@@ -1,23 +1,96 @@
 import React from 'react'
-import {Flag} from 'semantic-ui-react'
-import OsnovaDropdown from 'components/OsnovaDropdown'
+import {Flag, Search, Icon, Button} from 'semantic-ui-react'
 import {countries} from 'country-data'
+import {autobind} from 'core-decorators'
 
 const options = countries.all
   .filter(c => c.status === 'assigned' && Flag._meta.props.name.indexOf(c.alpha2.toLowerCase()) !== -1) // eslint-disable-line no-underscore-dangle
   .map(c => ({
     value: c.alpha2.toLowerCase(),
-    name: c.name,
-    text: c.name
+    title: c.name
   }))
 
-const CountrySelect = (props) => (<OsnovaDropdown
-  selection
-  fluid
-  search
-  filter={(oo, searchText) => oo.filter(o => o.name.toLowerCase().startsWith(searchText.toLowerCase()))}
-  options={options}
-  {...props}
-/>)
-export default CountrySelect
+@autobind
+export default class CountrySelect extends React.Component {
 
+  static propTypes = {
+    value: React.PropTypes.any,
+    onFocus: React.PropTypes.func,
+    onChange: React.PropTypes.func,
+  }
+
+  static defaultProps = {
+    onFocus: () => {},
+    onChange: () => {}
+  }
+
+  componentWillMount() {
+    this.onNewValue(this.props.value)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.onNewValue(nextProps.value)
+  }
+
+  onSearchChange(e, searchText) {
+    this.setState({
+      results: this.searchByText(searchText),
+      label: searchText
+    })
+  }
+
+  searchByText(text) {
+    if (!text) {
+      return options.slice(0, 5)
+    }
+    return options.filter(o => o.title.toLowerCase().startsWith(text.trim().toLowerCase())).slice(0, 5)
+  }
+
+  label(value) {
+    if (!value) {
+      return ' '
+    }
+    if (value.length !== 2) {
+      return value
+    }
+    const r = options.find(o => o.value === value)
+    if (r) {
+      return r.title
+    }
+    return value || ' '
+  }
+
+  onNewValue(value) {
+    const label = this.label(value)
+    const results = this.searchByText(label)
+    this.setState({results, label})
+  }
+
+  setNull(e) {
+    e.preventDefault()
+    this.setState({
+      results: options.slice(0, 5),
+      label: ' ',
+    })
+    this.props.onChange(e, {value: ''})
+  }
+
+  render() {
+    const {
+      value, // do not pass
+      onFocus, // do not pass
+      ...rest
+    } = this.props
+
+    return (<Search
+      selectFirstResult
+      results={this.state.results}
+      onSearchChange={this.onSearchChange}
+      value={this.state.label}
+      icon={<Button icon='erase' basic onClick={this.setNull} />}
+      {...(rest)}
+    />)
+  }
+
+
+}
